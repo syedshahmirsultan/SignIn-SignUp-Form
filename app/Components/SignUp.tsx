d03ea9a97e4c22bbbfd9be777fc0f246c32cdc4d
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-07-08 23:21:36 +0500

    updated code

--- app/Components/SignUp.tsx
+++ app/Components/SignUp.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SignUp = () => {
     return (
-        <div>
-         <h1>Sign Up form</h1>  
-        </div>
+        <section className="mt-20 bg-gray-100/50 shadow-md border h-96 w-72 border-gray-100">
+         <h1 className='text-gray-950 font-semibold text-xl text-center mt-4'>SignUp</h1>
+        </section>
     );
 }
 
