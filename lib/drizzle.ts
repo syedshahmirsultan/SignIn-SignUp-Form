d120ef6a9944ed2853ec465fd814000c29589af5
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-07-09 00:34:34 +0500

    setup drizzle orm and postgresql database

---
+++ lib/drizzle.ts
@@ -0,0 +1,13 @@
+import {drizzle} from 'drizzle-orm/vercel-postgres'
+import {sql} from '@vercel/postgres'
+import {  pgTable, varchar } from 'drizzle-orm/pg-core'
+
+
+export const userDataTable = pgTable("authenticationtale",{
+    username :varchar('username', {length : 255}).notNull(),
+    useremail:varchar('useremail', {length : 255}).notNull().unique(),
+    password: varchar('password', {length : 255}).notNull(),
+})
+
+
+export const db = drizzle(sql);
